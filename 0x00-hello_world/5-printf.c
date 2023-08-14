15adf57a522021f287db6d6e1cb4275270fff9e5
lindamndau <clavinmndawe@gmail>
2023-08-14 16:57:54 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - A program that prints a string with a new line
  * Returns: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
