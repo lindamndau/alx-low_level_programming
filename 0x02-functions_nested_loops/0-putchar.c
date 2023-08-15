2e127be61f8a5e41b241e84e5c629886a9689447
lindamndau <clavinmndawe@gmail>
2023-08-15 15:36:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints _putchar, followed by a new line.
+ * Return (0); Success
+ */
+
+int main(void)
+{
+	// Data type declaration and initiallization//
+	char putchar[] = "_putchar";
+	// using printf to print string//
+	printf("%s _putchar\n", putchar);
+	return (0);
+}
