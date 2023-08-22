81581be9a9f6ac52e701c3aac3f641870f2ccb6d
lindamndau <clavinmndawe@gmail>
2023-08-22 16:30:06 -0700

    Nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_HEADERR_FILE
+#ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-
-int _putchar (char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,6 +11,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n)
+void print_to_98(int n);
 void print_times_table(int n);
 #endif
