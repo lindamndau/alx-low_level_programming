8bef258a4d6f5b9b9fcd617702152fbd2d8de689
lindamndau <clavinmndawe@gmail>
2023-08-22 14:51:40 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - A program that prints _putchar, followed by a new line.
- * Return (0); Success
- */
+* main - A program that prints _putchar, followed by a new line.
+* Return (0); Success
+*/
 
 int main(void)
 {
 	// Data type declaration and initiallization//
 	char putchar[] = "_putchar";
-	// using printf to print string//
-	printf("%s _putchar\n", putchar);
+	// using write to print string and newline//
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
