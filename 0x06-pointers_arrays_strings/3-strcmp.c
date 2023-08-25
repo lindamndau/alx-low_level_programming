c156fc17663677f707a22ed7cf2b8fe768422025
lindamndau <clavinmndawe@gmail>
2023-08-25 07:28:15 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, c;
-	for(; s1[i] != '\0'; && s2[j] != '\0'; i++, j++)
+	int i = 0;
+	while(; s1[i] != '\0'; &&  s2[i] != '\0' i++)
 	{
-		c = (s1[i] - s2[j]);
-	}
-	if(c == 0)
-	{
-		return(0);
-	}
-	else 
-		return (c);
+		if(s1[i] == s2[i])
+		{
+			return (0);
+			
+		}
+	      	return (s1[i] - s2[i]);
+	
 }
