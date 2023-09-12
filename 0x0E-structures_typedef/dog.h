714d7b0653b553be6c46777cbb8f5237ae3856b4
lindamndau <clavinmnadwe@gmail.com>
2023-09-12 15:04:40 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 /**
  * This structure define a struct with three elelments
  * name .type = char
  * age .type =  float
  * owner .type =  char
  */
-typedef struct dog 
+typedef struct dog
 {
-	char  *name;
+	char* name;
 	float age;
-       char *owner;
-}my_dog;
+	char* owner;
+}dog;
