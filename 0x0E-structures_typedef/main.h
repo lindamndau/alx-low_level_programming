26adbf4b215f6da047a8a3a389194123e27b1b9c
lindamndau <clavinmnadwe@gmail.com>
2023-09-12 16:59:01 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_HEADRE_FILE
+#define _MAIN_HEADRR_FILE
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void _putchar(int c);
+#endif
