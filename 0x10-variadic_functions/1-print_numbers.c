4584ed0f3550d5c0b6b40525c7665817ca99106b
lindamndau <clavinmnadwe@gmail.com>
2023-09-15 18:42:46 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,22 +10,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list nums;
-	unsigned int index;
+	va_list valist;
+	unsigned int i;
 
-	va_start(nums, n);
+	va_start(valist, n);
 
-	for (index = 0; index < n; index++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(nums, int));
-
-		if (index != (n - 1) && separator != NULL)
-		{
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
 			printf("%s", separator);
-		}
 	}
 
 	printf("\n");
-
-	va_end(nums);
+	va_end(valist);
 }
