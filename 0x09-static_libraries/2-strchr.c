8789058f0710940ab47278ae1c84f3126e37d10f
lindamndau <clavinmndawe@gmail.com>
2023-10-29 11:47:07 -0700

    Static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else if (*(s + 1) == c)
+		{
+			return (s + 1);
+		}
+		s++;
+	}
+	return (s + 1);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
