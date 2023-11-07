76a22636fc563d9024bbee416d37a49fa81480c8
lindamndau <clavinmndawe@gmail.com>
2023-11-06 16:00:42 -0800

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This program prints the name of the file it was compiled from
+ *
+ *
+ *
+ *
+ */
+int main()
+{
+	printf("%s\n", _FILE_);
+	return 0;
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef SUM
+#define SUM
+
+#define sum(x, y) ((x + y))
+#endif
