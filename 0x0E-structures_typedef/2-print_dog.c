74b3b1244f28274ee147ef0545514eff3eb57566
lindamndau <clavinmndawe@gmail.com>
2023-11-07 05:23:40 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,14 +14,14 @@ void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		if ((*d).name != '\0')
+		if ((*d).name != NULL)
 			printf("Name: %s\n", (*d).name);
 		else
 		{
 			printf("Name: (NULL)\n");
 		}
 		printf("Age: %f\n", (*d).age);
-		if ((*d).owner != '\0')
+		if ((*d).owner != NULL)
 		{
 			printf("Owner: %s\n", (*d).owner);
 		}
