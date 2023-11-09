03d0e8a05d902ab12022b48eab7bf2a7cecad001
lindamndau <clavinmndawe@gmail.com>
2023-11-08 16:41:59 -0800

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
  * @param name: A pointer to a string, representing the name to be checked.
  * @param f: A function pointer to the function to be called.
  */
+void print_name(char *name, void (*f)(char *))
 {
 	if(name == NULL)
 	{
--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	n = atoi(argv[1]);
+	num = atoi(argv[1]);
 	if (num < 0)
 	{
 		printf("Error\n");
