7dc382cd553ba39a8b4007ee40f623bf564b3e51
lindamndau <clavinmndawe@gmail.com>
2023-11-24 12:06:23 -0800

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * @Discription -  converts a binary number to an unsigned int.
  * Returns : converted string
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * @Discription - returns the number of bits you would need to flip to get from one number to another.
  * @flip_count - counter to keep track of the number of bits to flip
@@ -7,12 +6,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int difference = original_number ^ target_number;
-	int flip_count = 0;
-	while (difference)
+	unsigned long int diff;
+	int flip_count;
+
+	diff = n ^ m;
+	flip_count = 0;
+
+	while (diff)
 	{
 		flip_count++;
-		difference &= (difference - 1);
+		diff &= (diff - 1);
 	}
-	 return flip_count;
+
+	return (flip_count);
 }
